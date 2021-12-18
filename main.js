bb3691020160b5b77da9db5e0eba94e2df841805
Robert-Napier <84276666+Robert-Napier@users.noreply.github.com>
2021-12-18 13:29:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+Webcam.attach( '#camera' );
+
+camera = document.getElementById("camera");
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+});
+
+function snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('m15 version:', m15.version);
+
+classifier = m15.imageClassifier('https://teachablemachine.withgoogle.com/models/cIpmMhk5m/model.json',modelLoaded);
+
+
+function modelLoaded(){
+    console.log('Model Loaded!');  
+}
+
+function check(){
+    img = document.getElementById('selfie_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error){
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
